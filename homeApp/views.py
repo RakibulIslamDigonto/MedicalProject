52ecacbfa41a8d347c8a71d7f40dd17fad4c2b69
Rakibul Islam Digonto <70770224+RakibulIslamDigonto@users.noreply.github.com>
2021-12-25 20:25:57 +0600

    nothing updated

--- homeApp/views.py
+++ homeApp/views.py
@@ -7,36 +7,31 @@ from django.contrib import messages
 
 # Create your views here.
 class HomeView(View):
-    temp_name='homeApp/index.html'
+    temp_name = 'homeApp/index.html'
 
     def get(self, request):
         slides = SlideModel.objects.all().order_by('-id')[:3]
         skills = Skill.objects.all().order_by('-id')
         services = Services.objects.all().order_by('-id')
         departments = Department.objects.all()
-        department_gals = Department_galary.objects.filter(department__in=departments)
+        department_gals = Department_galary.objects.filter(
+            department__in=departments)
         appoinment = AppoinmentForm
 
-        context ={
+        context = {
             'slides': slides,
             'skills': skills,
             'services': services,
             'appoinment': appoinment,
             'departments': departments,
             'department_gals': department_gals
-            }
+        }
         return render(request, self.temp_name, context)
-
 
     def post(self, request):
         appoinment = AppoinmentForm(request.POST)
         if appoinment.is_valid():
             appoinment.save()
-            messages.success(request, 'Profile details updated.')
+            messages.success(request, 'Congrats! you appointed.')
             return redirect('homeApp:home')
         return render(request, self.temp_name, {'appoinment': appoinment})
-
-
-
-
-
